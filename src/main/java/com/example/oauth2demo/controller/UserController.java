001a4e1be6732e23e96492205b682c378f177fd7
Jenish Bajracharya <jenish.bajracharya@suvidhatech.com>
2018-08-09 17:35:42 +0545

    logout(revoke access token)

--- src/main/java/com/example/oauth2demo/controller/UserController.java
+++ src/main/java/com/example/oauth2demo/controller/UserController.java
@@ -3,6 +3,9 @@ package com.example.oauth2demo.controller;
 import com.example.oauth2demo.model.UserModel;
 import com.example.oauth2demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.provider.token.ConsumerTokenServices;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,8 +17,15 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    BCryptPasswordEncoder passwordEncoder;
+
+    @Autowired
+    ConsumerTokenServices tokenServices;
+
     @RequestMapping(value= "/create", method = RequestMethod.POST)
     public UserModel saveUser(@RequestBody UserModel userModel){
+        userModel.setPassword(passwordEncoder.encode(userModel.getPassword()));
         return userService.saveUser(userModel);
     }
 
@@ -29,6 +39,15 @@ public class UserController {
         userService.deleteUser(id);
         return "success";
     }
+
+
+    @RequestMapping(value = "/logout/{tokenId}", method = RequestMethod.POST)
+    public String revokeToken(@PathVariable String tokenId){
+        tokenServices.revokeToken(tokenId);
+        return tokenId;
+    }
+
+
 
 
 }
